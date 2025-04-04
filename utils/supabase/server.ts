ffeb6a5f8522802fa59582df5edc88ac2901d53f
Michael Fauve <michael.fauve@santarel.com>
2025-04-04 14:13:57 +0200

    ✅ Fix Supabase SSR pour Vercel

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,13 +1,20 @@
-import { cookies, headers } from 'next/headers';
-import { createServerClient } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr'
+import { cookies, headers } from 'next/headers'
 
 export const createClient = () => {
+  const cookieStore = cookies()
+  const headerStore = headers()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: cookies(),
-      headers: headers(),
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value
+        },
+      },
+      headers: () => headerStore,
     }
-  );
-};
+  )
+}
